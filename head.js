4916305935dffbab306e7251a61f2ead1dae92e6
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-18 23:58:48 -0500

    Created 'head' function which returns the first element (head) in an array

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = function(array){
+  return array[0];
+};
+
+assertEqual(head([4,5,1]), 4);
+assertEqual(head([4,5,1]), 1);
+assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "LHL");
+assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "Bootcamp");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
+assertEqual(head([1, 1, 1, 1]), 1);
