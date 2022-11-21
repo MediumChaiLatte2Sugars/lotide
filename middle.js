6e4bfafa49cbe112562f5cb3b08105b4f9464220
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-20 20:36:41 -0500

    Implemented middle with helper function

--- middle.js
+++ middle.js
@@ -14,6 +14,32 @@ const eqArrays = function(array1, array2) {
 
 };
 
+const pushToArray = function(ogArray, midArray, indices){
+  if (indices.length === 1){
+    midArray.push(ogArray[indices[0]]);
+  }
+
+  for (let number of indices){
+    midArray.push(ogArray[number]);
+  }
+
+  return midArray;
+}
+
 const assertArraysEqual = function(array1, array2) {
   (eqArrays(array1, array2)) ? console.log(`✅✅✅💃Assertion Passed: ${array1} === ${array2}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${array1} !== ${array2}`);
+};
+
+const middle = function(sourceArray){
+  let middleArray = [];
+
+  switch (sourceArray.length % 2){
+    case 0:
+      middleArray = pushToArray(sourceArray, middleArray, [(sourceArray.length / 2) - 1, sourceArray.length / 2]);
+      break;
+    case 1:
+      middleArray = pushToArray(sourceArray, middleArray, [Math.ceil(sourceArray.length / 2)]);
+  }
+
+  return middleArray;
 };
