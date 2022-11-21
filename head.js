40882350001048470fb76b3b94ba7d92833fc3a9
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-20 21:05:55 -0500

    Ran through linter and made some changes

--- head.js
+++ head.js
@@ -2,12 +2,12 @@ const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const head = function(array){
+const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([4,5,1]), 4);
-assertEqual(head([4,5,1]), 1);
+assertEqual(head([4, 5, 1]), 4);
+assertEqual(head([4, 5, 1]), 1);
 assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "LHL");
 assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "Bootcamp");
 assertEqual(head([1]), 1);
