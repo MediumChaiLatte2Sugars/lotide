8bcfab5aa7acfa2e89f47ff2e14b47581d5c3d5b
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-20 20:52:28 -0500

    Completed middle.js

--- middle.js
+++ middle.js
@@ -48,3 +48,15 @@ const middle = function(sourceArray){
 
   return middleArray;
 };
+
+let testArray1 = [1, 2, 3, 4, 5];
+assertArraysEqual(middle(testArray1), [3]);
+
+testArray1 = [1, 2, 4, 5];
+assertArraysEqual(middle(testArray1), [2, 4]);
+
+testArray1 = [1];
+assertArraysEqual(middle(testArray1), [1]);
+
+testArray1 = [];
+assertArraysEqual(middle(testArray1), []);
