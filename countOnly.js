7a9d4c9c16e59e3db004a2dc7d5abf3bfc0543f0
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-21 19:58:54 -0500

    Added test case

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,25 @@
 const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const countOnly = function(allItems, itemsToCount){
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
