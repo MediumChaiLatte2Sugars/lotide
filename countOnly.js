92c7a81c85f0bc65b014da8d5da161f76cb19953
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-21 19:38:04 -0500

    Copied over assertEqual

---
+++ countOnly.js
@@ -0,0 +1,3 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
+};
