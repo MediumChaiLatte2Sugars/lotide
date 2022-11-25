8157e06bf190691ab0f200b285dc96c6e52929dd
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-25 01:31:04 -0500

    Copied over eqArrays, added more tests

--- eqObjects.js
+++ eqObjects.js
@@ -2,6 +2,21 @@ const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
 };
 
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
 
   if (Object.keys(object1).length !== Object.keys(object2).length || Object.values(object1).length !== Object.values(object2).length) {
@@ -27,6 +42,7 @@ const eqObjects = function(object1, object2) {
 
 };
 
+// Tests for prim's
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
@@ -42,3 +58,21 @@ assertEqual(abc.a, ab.a);
 assertEqual(abc.b, ab.b);
 assertEqual(abc.c, ab.c);
 assertEqual(Object.keys(abc).length, Object.keys(ab).length);
+
+// non prim's test
+console.log("NON PRIM TESTS");
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+assertEqual(cd.c, dc.c);
+assertEqual(cd.d, dc.d);
+assertEqual(Object.keys(cd).length, Object.keys(dc).length);
+
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+
+assertEqual(cd.c, cd2.c);
+assertEqual(cd.d, cd2.d);
+assertEqual(Object.keys(cd).length, Object.keys(cd2).length);
