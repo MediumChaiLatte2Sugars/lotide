cc32839a667ae36dfb4815b19a0134e7c5484ab3
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-25 00:30:08 -0500

    Implemented barebones findKeyByValue with test

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,18 @@
 const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const findKeyByValue = function(object, value){
+  let firstFoundKey;
+
+  return firstFoundKey;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
