b5c55883f786e34b9e2fbf0a9113c3e2e1830e54
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-24 23:52:26 -0500

    Created countLetters (barebones) function w/ test

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,16 @@
 const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const countLetters = function(sourceString){
+  letterCount = {};
+
+  return letterCount;
+};
+
+let testString = "JHGL"
+let testCounter = countLetters(testString);
+assertEqual(testCounter.J, 1);
+assertEqual(testCounter.H, 1);
+assertEqual(testCounter.G, 1);
+assertEqual(testCounter.L, 1);
