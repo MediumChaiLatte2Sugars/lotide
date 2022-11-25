e3b6338d7e64e065329b9919fe64a2855cb7d2ac
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-25 00:07:09 -0500

    Copied over assertEqual & assertArraysEqual

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,10 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const assertArraysEqual = function(array1, array2) {
+  (eqArrays(array1, array2)) ? console.log(`✅✅✅💃Assertion Passed: ${array1} === ${array2}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${array1} !== ${array2}`);
+};
 
 const letterPositions = function(sentence){
   const results = {};
