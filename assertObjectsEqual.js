9a30d260d4faa42b8b4bc4cfc43311dab895136c
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-25 22:49:20 -0500

    Copied over eqArrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,18 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
 
   // Checks if objects have the same number of keys
@@ -16,7 +31,7 @@ const eqObjects = function(object1, object2) {
 
     // Check to see if the values are the same (Arrays)
     if (Array.isArray(object1[object1KeyAtIndex]) && Array.isArray(object2[object2KeyAtIndex])) {
-      return eqArrays(object1[object1KeyAtIndex], object2[object2KeyAtIndex])
+      return eqArrays(object1[object1KeyAtIndex], object2[object2KeyAtIndex]);
     }
 
     // Check to see if the values are the same (primitives)
@@ -27,13 +42,17 @@ const eqObjects = function(object1, object2) {
   }
 
   return true;
-}
+};
 
-const assertObjectsEqual = function(object1, object2){
+const assertObjectsEqual = function(object1, object2) {
 
   const inspect = require('util').inspect;
-  (eqObjects(object1, object2)) ?  console.log(`✅✅✅💃Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
-}
+  (eqObjects(object1, object2)) ? console.log(`✅✅✅💃Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
 
-let testObject = {1: "one", 2: "two"}, testObject2 = {1: "one", 2: "three"};
+};
+
+let testObject = { 1: "one", 2: "two" }, testObject2 = { 1: "one", 2: "three" };
+assertObjectsEqual(testObject, testObject2);
+
+testObject = { 1: "one", 2: "two" }, testObject2 = { 1: "one", 2: "two" };
 assertObjectsEqual(testObject, testObject2);
