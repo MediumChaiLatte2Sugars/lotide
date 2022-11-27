fc44260681f6251410ec2822a400cd60a147c264
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-26 21:47:53 -0500

    Implenting for loop testing callback mutation

--- map.js
+++ map.js
@@ -1,6 +1,14 @@
 
-const map = function(array, cBFunction){
-  //empty
+const map = function(array, callback){
+  const results = [];
+
+  for (let item of array){
+    console.log("item BEFORE:", item);
+    console.log("item AFTER:", callback(item));
+    console.log("---")
+  }
+
+  return results;
 };
 
 const words = ["ground", "control", "to", "major", "tom"];
