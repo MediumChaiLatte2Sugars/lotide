d08ae5e87df88f96852b4914e7630c9c6caa2938
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-26 22:20:06 -0500

    Implemented takeUntil, added addn'l tests

--- takeUntil.js
+++ takeUntil.js
@@ -18,8 +18,19 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 const takeUntil = function(array, callback) {
-  // ...
-}
+  let resultArray = [];
+
+  for (let item of array){
+    
+    if (callback(item)){
+      return resultArray;
+    }
+
+    resultArray.push(item);
+  }
+
+  return resultArray;
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -30,3 +41,11 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+let testArray = [1, 2, 3, 5, 6];
+let resultArray = takeUntil(testArray, number => number > 4);
+assertArraysEqual(resultArray, [1, 2, 3]);
+
+testArray = [1, 2, , 3, 5, , 6, 0];
+resultArray = takeUntil(testArray, number => number === 4);
+assertArraysEqual(resultArray, [1, 2, , 3, 5, , 6, 0]);
