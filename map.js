f289225c9323f1c8278d216c2c2b07f19eee14dd
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-26 21:43:27 -0500

    Implemented barebones map w/ test

---
+++ map.js
@@ -0,0 +1,9 @@
+
+const map = function(array, cBFunction){
+  //empty
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
