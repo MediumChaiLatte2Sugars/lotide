6f75a024535ce9579a2c3a2f07dba0a967689dab
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-09 15:44:44 -0500

    Exported additional utility functions

--- index.js
+++ index.js
@@ -2,9 +2,27 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  without: without,
+  takeUntil: takeUntil,
+  letterPositions: letterPositions,
+  flatten: flatten,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  eqObjects: eqObjects,
+  countOnly: countOnly,
+  countLetters: countLetters,
 };
