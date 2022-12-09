391d55cef80bb5cb82deaa4c350f4e8600c75632
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-09 00:00:34 -0500

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,3 @@
-const assertEqual = function(actual, expected) {
-  (actual === expected) ? console.log(`✅✅✅💃Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑🕵️‍♀️Assertion Failed: ${actual} !== ${expected}`);
-};
 
 const eqArrays = function(array1, array2) {
 
@@ -17,14 +14,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-let testArray1 = [1, 3, 5], testArray2 = [2, 4, 5];
-assertEqual(eqArrays(testArray1, testArray2), false);
-
-testArray1 = [2, 4, 5], testArray2 = [2, 4, 5];
-assertEqual(eqArrays(testArray1, testArray2), true);
-
-testArray1 = [], testArray2 = [];
-assertEqual(eqArrays(testArray1, testArray2), true);
-
-testArray1 = [2, 4, 5, 6], testArray2 = [2, 4, 5];
-assertEqual(eqArrays(testArray1, testArray2), false);
+module.exports = eqArrays;
