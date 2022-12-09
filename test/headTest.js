29c880433c2c68d51ba25aed34dbc0023f32dff8
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-09 00:25:18 -0500

    Refactored headTest.js to incl. mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,21 @@
 const head = require('../head');
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual(head([4, 5, 1]), 4);
-assertEqual(head([4, 5, 1]), 1);
-assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "LHL");
-assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "Bootcamp");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
-assertEqual(head([1, 1, 1, 1]), 1);
+// assertEqual(head([4, 5, 1]), 4);
+// assertEqual(head([4, 5, 1]), 1);
+// assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "LHL");
+// assertEqual(head(["LHL", "Bootcamp", "Web Dev FLEX"]), "Bootcamp");
+// assertEqual(head([1]), 1);
+// assertEqual(head([]), undefined);
+// assertEqual(head([1, 1, 1, 1]), 1);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
